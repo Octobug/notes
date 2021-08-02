e7215c1f71ac3390c5b4ee3225da34582eae2eac
ShaobiaoLin <linshaobiao@minieye.cc>
2021-08-02 19:22:43 +0800

    se(st): added promise async test

---
+++ software_engineering/software_testing/fundamentals/testing-async-code/tests/async/promise.test.js
@@ -0,0 +1,30 @@
+/* global describe, it */
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-as-promised'));
+
+// This is just an async func that takes in a bool
+// and that returns a promise
+function someMadeUpAyncFunc(boolValue) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(boolValue ? 'You get a sweet :)' : 'You get nothing!!');
+    }, 0);
+  });
+}
+
+describe('AsyncTest', function () {
+  it('should return `You get a sweet :)` if `true` is passed in',
+    function () {
+      return expect(someMadeUpAyncFunc(true)).to.eventually.equal(
+        'You get a sweet :)'
+      );
+    });
+
+  it('should return `You get nothing!!` if `false` is passed in',
+    function () {
+      return expect(someMadeUpAyncFunc(false)).to.eventually.equal(
+        'You get nothing!!'
+      );
+    });
+});
