88f017a9ce6651ce8bf0a1d88995dd6dd5510834
Octobug <whalevocal@gmail.com>
2021-08-15 17:56:41 +0800

    algo(leetcode): solved 1313

---
+++ algorithms/leetcode/easy/1313_decompress_run_length_encoded_list/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> decompressRLElist(vector<int> &nums)
+    {
+        int size = 0;
+        for (int i = 0; i < nums.size(); i += 2)
+        {
+            size += nums[i];
+        }
+
+        vector<int> ans(size);
+        int count = 0;
+        for (int i = 0; i < nums.size(); i += 2)
+        {
+            fill(ans.begin() + count,
+                 ans.begin() + count + nums[i],
+                 nums[i + 1]);
+            count += nums[i];
+        }
+
+        return ans;
+    }
+};
+
+void output(vector<int> &arr)
+{
+    cout << '[';
+    cout << boolalpha;
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        cout << arr[i];
+        if (i != arr.size() - 1)
+        {
+            cout << ',';
+        }
+    }
+    cout << ']' << endl;
+}
+
+int main()
+{
+    Solution s;
+
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> ans = s.decompressRLElist(nums);
+    output(ans);
+
+    nums = {1, 1, 2, 3};
+    ans = s.decompressRLElist(nums);
+    output(ans);
+}
