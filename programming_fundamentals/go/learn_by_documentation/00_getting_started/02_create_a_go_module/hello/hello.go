678b3a6a78ceac2f386eb92ce4a2d7c0d26d6666
Octobug <whalevocal@gmail.com>
2021-08-20 12:17:16 +0800

    pf(go): finished error handling

--- programming_fundamentals/go/learn_by_documentation/00_getting_started/02_create_a_go_module/hello/hello.go
+++ programming_fundamentals/go/learn_by_documentation/00_getting_started/02_create_a_go_module/hello/hello.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable priting
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("Gladys")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+
+	// Request a greeting message.
+	message, err = greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
