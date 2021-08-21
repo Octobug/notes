d6b1382bd866c5013a122bbfe76ca2b5e7bca271
Octobug <whalevocal@gmail.com>
2021-08-21 12:34:51 +0800

    algo(leetcode): added 1302

---
+++ algorithms/leetcode/medium/1302_deepest_leaves_sum/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    int deepestLeavesSum(TreeNode *root)
+    {
+    }
+};
+
+int main()
+{
+}
