9499fb080518424184ef8d89150aef28c73a006e
Octobug <whalevocal@gmail.com>
2023-07-08 18:07:57 +0800

    lang(index): update langs ranking

--- linguistics/second_language_acquisition/README.md
+++ linguistics/second_language_acquisition/README.md
@@ -6,33 +6,47 @@
 
 [Duolingo](https://www.duolingo.com/)
 
-## Influence of Languages
+## Influence Index of Languages
 
 ```txt
-English: 75
-Chinese: 38
-German: 34
-Japanese: 33
-Spanish: 29
-Russian: 26
-French: 25
-Portuguese: 21
-Italian: 20
-Arabic: 19
-Malay-Indonesian: 17
-Korean: 16
-Hindi: 16
-Persian: 14
-Turkish: 11
-Dutch: 9
-Polish: 6
-Thai: 5
-Vietnamese: 3
-Punjabi: 2
-Bengali: 1
+English: 100.00
+Chinese: 60.88
+Spanish: 60.72
+French: 56.39
+German: 52.63
+Japanese: 51.33
+Italian: 44.91
+Portuguese: 40.69
+Korean: 38.18
+Arabic: 38.18
+Russian: 37.56
+Dutch: 32.76
+Hindi: 31.92
+Malay-Indonesian: 29.58
+Turkish: 28.79
+Polish: 23.16
+Swedish: 20.03
+Bengali: 18.78
+Thai: 17.53
+Farsi: 13.77
+Vietnamese: 12.52
+Norwegian: 12.21
+Punjabi: 10.02
+Hebrew: 8.76
+Danish: 8.76
+Javanese: 6.26
+Greek: 5.01
+Tagalog: 3.76
+Romanian: 2.50
+Romansh: 1.72
+Irish: 1.56
+Finnish: 1.25
+Sami: 0.94
+Tamil: 0.16
 ```
 
 Based on these rankings:
 
-- [The 20 languages that produce 82% of the world's GDP (2021 update)](https://www.reddit.com/r/languagelearning/comments/rs241i/the_20_languages_that_produce_82_of_the_worlds/)
 - [List of countries by GDP (nominal)](https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal))
+- [World languages by GDP, 2023 edition](https://www.reddit.com/r/languagelearning/comments/11xt73g/world_languages_by_gdp_2023_edition/)
+  - [The 20 languages that produce 82% of the world's GDP (2021 update)](https://www.reddit.com/r/languagelearning/comments/rs241i/the_20_languages_that_produce_82_of_the_worlds/)
--- linguistics/second_language_acquisition/ranking.js
+++ linguistics/second_language_acquisition/ranking.js
@@ -2,92 +2,143 @@ const LANG = {
   ARABIC: 'Arabic',
   BENGALI: 'Bengali',
   CHINESE: 'Chinese',
+  DANISH: 'Danish',
   DUTCH: 'Dutch',
   ENGLISH: 'English',
+  FARSI: 'Farsi',
+  FINNISH: 'Finnish',
   FRENCH: 'French',
   GERMAN: 'German',
+  GREEK: 'Greek',
+  HEBREW: 'Hebrew',
   HINDI: 'Hindi',
+  IRISH: 'Irish',
   ITALIAN: 'Italian',
   JAPANESE: 'Japanese',
+  JAVANESE: 'Javanese',
   KOREAN: 'Korean',
   MALAY_INDONESIAN: 'Malay-Indonesian',
+  NORWEGIAN: 'Norwegian',
   PERSIAN: 'Persian',
   POLISH: 'Polish',
   PORTUGUESE: 'Portuguese',
   PUNJABI: 'Punjabi',
+  ROMANIAN: 'Romanian',
+  ROMANSH: 'Romansh',
   RUSSIAN: 'Russian',
+  SAMI: 'Sami',
   SPANISH: 'Spanish',
+  SWEDISH: 'Swedish',
+  TAGALOG: 'Tagalog',
+  TAMIL: 'Tamil',
   THAI: 'Thai',
   TURKISH: 'Turkish',
   VIETNAMESE: 'Vietnamese',
 };
 
 const rankByLangGDP = [
+  // 01-10
   LANG.ENGLISH,
   LANG.CHINESE,
   LANG.SPANISH,
-  LANG.GERMAN,
-  LANG.ARABIC,
   LANG.JAPANESE,
-  LANG.RUSSIAN,
-  LANG.MALAY_INDONESIAN,
-  LANG.PORTUGUESE,
+  LANG.GERMAN,
   LANG.FRENCH,
-  LANG.TURKISH,
+  LANG.ARABIC,
   LANG.ITALIAN,
+  LANG.PORTUGUESE,
   LANG.KOREAN,
+  // 11-20
+  LANG.RUSSIAN,
+  LANG.HINDI,
   LANG.DUTCH,
+  LANG.TURKISH,
+  LANG.MALAY_INDONESIAN,
+  LANG.BENGALI,
   LANG.POLISH,
+  LANG.SWEDISH,
   LANG.THAI,
-  LANG.PERSIAN,
+  LANG.FARSI,
+  // 21-30
   LANG.VIETNAMESE,
+  LANG.NORWEGIAN,
   LANG.PUNJABI,
-  LANG.BENGALI
+  LANG.DANISH,
+  LANG.HEBREW,
+  LANG.JAVANESE,
+  LANG.GREEK,
+  LANG.TAGALOG,
+  LANG.ROMANIAN,
+  LANG.FINNISH,
 ];
 
 const rankByNationGDP = [
+  // 01-10
   LANG.ENGLISH,
   LANG.CHINESE,
   LANG.JAPANESE,
   LANG.GERMAN,
-  LANG.HINDI,
+  [LANG.HINDI, LANG.ENGLISH],
   LANG.ENGLISH,
   LANG.FRENCH,
-  LANG.ENGLISH,
-  LANG.RUSSIAN,
   LANG.ITALIAN,
-  LANG.PERSIAN,
+  [LANG.ENGLISH, LANG.FRENCH],
   LANG.PORTUGUESE,
+  // 11-20
+  LANG.RUSSIAN,
   LANG.KOREAN,
   LANG.ENGLISH,
   LANG.SPANISH,
   LANG.SPANISH,
   LANG.MALAY_INDONESIAN,
-  LANG.ARABIC,
   LANG.DUTCH,
-  LANG.TURKISH
+  LANG.ARABIC,
+  LANG.TURKISH,
+  [LANG.GERMAN, LANG.FRENCH, LANG.ITALIAN, LANG.ROMANSH],
+  // 21-30
+  LANG.CHINESE,
+  LANG.POLISH,
+  LANG.SPANISH,
+  [LANG.DUTCH, LANG.FRENCH, LANG.GERMAN],
+  LANG.SWEDISH,
+  [LANG.IRISH, LANG.ENGLISH],
+  LANG.THAI,
+  [LANG.NORWEGIAN, LANG.SAMI],
+  LANG.HEBREW,
+  [LANG.ENGLISH, LANG.MALAY_INDONESIAN, LANG.CHINESE, LANG.TAMIL],
 ];
 
 const scores = {};
 
 rankByLangGDP.slice(0).reverse().forEach((lang, index) => {
-  const score = index + 1;
+  const score = (index + 1) * 2;
   scores[lang] = score;
 });
 
-rankByNationGDP.slice(0).reverse().forEach((lang, index) => {
-  const score = index + 1;
-  if (scores[lang]) {
-    scores[lang] += score;
-  } else {
-    scores[lang] = score;
+rankByNationGDP.slice(0).reverse().forEach((langs, index) => {
+  if (!(langs instanceof Array)) {
+    langs = [langs];
+  }
+
+  const score = (index + 1) / langs.length;
+  for (const lang of langs) {
+    if (scores[lang]) {
+      scores[lang] += score;
+    } else {
+      scores[lang] = score;
+    }
   }
 });
 
-Object.entries(scores).sort((a, b) => {
+
+const sorted = Object.entries(scores).sort((a, b) => {
   const [, scoreA] = a;
   const [, scoreB] = b;
   return scoreB - scoreA;
-}).forEach(([lang, score],) => {
-  console.log(`${lang}: ${score}`);
+});
+
+const coefficient100 = 100 / sorted[0][1];
+
+sorted.forEach(([lang, score],) => {
+  console.log(`${lang}: ${(score * coefficient100).toFixed(2)}`);
 });
