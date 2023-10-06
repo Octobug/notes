4b54ca58128b28fee13ef5509476a05a98e3eed9
Octobug <whalevocal@gmail.com>
2023-10-06 16:37:08 +0800

    algo(leetcode): solved 3 problems

---
+++ algorithms_and_data_structures/leetcode/pandas/e_02890_reshape_data_melt/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return report.melt(id_vars=["product"],
+                       var_name="quarter",
+                       value_name="sales")
